24f4ea9b90e1b4684fad3595726ca013e7cf357b
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2023-03-18 19:45:26 +0100

    we need shaders too ;)

---
+++ Hydra/src/Hydra/API/ResourceInterface/Shader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Hydra/API/Resource.h"
+namespace Hydra
+{
+	struct ShaderSpecification
+	{
+
+	};
+
+	class Shader : public Resource
+	{
+	public:
+		virtual ~Shader(){}
+	};
+}
