4c0720d5dddd25f0bc0d97431a5399dfb4c2208c
Niklas zzWeeZz Jakobsen <42831999+zzWeeZz@users.noreply.github.com>
2023-03-21 18:04:53 +0100

    fixes

--- AssetData/Shaders/Triangle_vs.hlsl
+++ AssetData/Shaders/Triangle_vs.hlsl
@@ -20,11 +20,12 @@ VertexOut main(VertexIn inVertex, uint vertexID : SV_VertexID)
     //    float4(0.0f, 1.0f, 0.0f, 1.0f),
     //    float4(1.0f, -1.0f, 0.0f, 1.0f)
     //};
-    float4 colours[3] =
+    float4 colours[4] =
     {
         float4(1.0f, 0.0f, 0.0f, 1.0f),
         float4(0.0f, 1.0f, 0.0f, 1.0f),
-        float4(0.0f, 0.0f, 1.0f, 1.0f)
+        float4(0.0f, 0.0f, 1.0f, 1.0f),
+        float4(1.0f, 0.0f, 1.0f, 1.0f)
     };
     inVertex.Position.x -= 0.5;
     outer.position = inVertex.Position;
